2915ca2bb77f05b607fcdda2662516af64d2c832
DasonTio <codingdec14@gmail.com>
2023-04-13 10:45:47 +0700

    navbar

---
+++ app/components/navbar/NavBar.tsx
@@ -0,0 +1,20 @@
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+
+const NavBar = () => {
+    return (
+        <nav className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b">
+                <Container>
+                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+                        <Logo />
+                        <Search />
+                    </div>
+
+                </Container>
+            </div>
+        </nav>
+    );
+}
+export default NavBar;
